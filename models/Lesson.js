be9a8a4ad9b6da25148f8465d4e74e2524159b75
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-05-05 21:23:56 -0400

    Setting up directories and files for API routes, data to seed

--- models/Lesson.js
+++ models/Lesson.js
@@ -1,14 +1,14 @@
 // lessons could be JSON strings with English words that we feed to the API
 // allow users to not only access our lessons, but create their own and set them to public / private
+// if public, other users can see them and try them out
 // id, words, date_created, created_by, public
 
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
-
 class Lesson extends Model {}
 
-Post.init(
+Lesson.init(
     {
         id: {
             type: DataTypes.INTEGER,
@@ -16,24 +16,25 @@ Post.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        title: {
+        words: {
             type: DataTypes.STRING,
             allowNull: false
-        },
-        content: {
-            type: DataTypes.TEXT,
         },
         date_created: {
             type: DataTypes.DATE,
             allowNull: false,
             defaultValue: DataTypes.NOW,
         },
-        user_id: {
+        created_by: {
             type: DataTypes.INTEGER,
             references: {
                 model: 'user',
                 key: 'id'
             }
+        },
+        public: {
+            type: DataTypes.BOOLEAN, 
+            allowNull: false,
         }
     },
     {
