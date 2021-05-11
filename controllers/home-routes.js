ec3c91512c9b60fd2613043762f0d3e7c0fae9bc
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-05-10 20:09:02 -0400

    Created several home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -13,4 +13,26 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+router.get('/user/', authorizeHelper, (req, res) => {
+    res.render('user');
+});
+
+router.get('/user/email', authorizeHelper, (req, res) => {
+    res.render('update_email');
+});
+
+router.get('/user/password', authorizeHelper, (req, res) => {
+    res.render('update_password');
+});
+
+router.get('/lesson', authorizeHelper, (req, res) => {
+    res.render('all_lessons');
+});
+
+router.get('/lesson/:id', authorizeHelper, (req, res) => {
+    // grab lesson id, send it to view
+    let lessonId = req.params.id;
+    res.render('single-lesson', lessonId);
+});
+
 module.exports = router;
