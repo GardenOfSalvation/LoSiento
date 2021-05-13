b6f6dd594413ee33ef7323a06216d09c43d4b3a7
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-05-12 21:21:19 -0400

    Added error handling for signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,7 +3,14 @@ async function signupUser(event) {
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const password2 = document.getElementById('password2').value;
     const email = document.getElementById('email').value;
+
+    if(password !== password2) {
+      document.getElementById('error_message').innerHTML = 'Passwords need to match!';
+      
+      return;
+    }
 
     const response = await fetch('/api/user/', {
         method: 'POST',
